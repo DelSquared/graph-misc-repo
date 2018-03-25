426525c6b2f247f9ef254fa29cc7962cfbc29ea7
DelSquared <37172200+DelSquared@users.noreply.github.com>
2018-03-25 22:53:44 +0200

    Update NetworkOfSquaresEntropy.py

--- NetworkOfSquares/NetworkOfSquaresEntropy.py
+++ NetworkOfSquares/NetworkOfSquaresEntropy.py
@@ -1,11 +1,17 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-n = 1000
+#This script explores a function of the degrees of vertices of a graph G: F(G)=SUM(ln(dv)/dv) where dv=deg(v). It is referred to as an
+#"Entropy" because by making a change of variables p=1/dv, we get F(G)=-SUM(p ln(p)) which is effectively the Shannon Entropy. Some
+#ready made plots and results can be found as PNG and CSV files along with this script up to graphs on 1000 vertices for the "sum of
+#pairs adding to squares" conjecture by Matt Parker. This script mainly tries to generate a statistic for small cases of such a graph
+#not an attempt to prove the conjecture.
 
-H=np.zeros((n))
+n = 100 #Number of vertices
 
+H=np.zeros((n)) #List of the "entropies"
 
+#Function to generate the adjacency matrix of a graph based on some rule.
 def G(n):
     B = np.zeros((n, n))
     for i in range(n):
@@ -15,7 +21,8 @@ def G(n):
                 B[j, i] = 1
     return B
 
-
+#Function to calculate the "entropies". Care was taken to navigate around divergences that could occur when a zero could slide
+#to the denominator
 def S(g):
     s = 0
     k = g.shape[0]
@@ -28,15 +35,16 @@ def S(g):
             s += np.log(w) / w
     return s
 
-
+#Loop calculating the "entropies"
 for _ in range(n):
     g = G(_)
     H[_]=S(g)
     if _%100==0:
-        print (_)
+        print ("current size being checked: ",_)
 
-np.savetxt("Graph Entropy-like functional up to 1000 vertices.csv",H,fmt = '%f' ,delimiter=',')
+#Output to CSV
+np.savetxt("Graph Entropy-like functional up to {} vertices.csv".format(n),H,fmt = '%f' ,delimiter=',')
 
-
-# plt.plot(H)
-# plt.show()
+#Plots
+plt.plot(H)
+plt.show()
